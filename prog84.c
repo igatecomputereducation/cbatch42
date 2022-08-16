30a527d71bf67b7f3df192e5ca03c40c3e51fa1e
igate solutions <igatecomputereducation@gmail.com>
2022-08-16 22:47:05 +0530

    v1

---
+++ prog84.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	if(10>20)
+		printf("one\n");
+	else if(40>=40)
+		printf("two\n");
+	else if(60<=60)
+		printf("three\n");
+	else if(40!=30)
+		printf("four\n");
+	return 0;
+}
